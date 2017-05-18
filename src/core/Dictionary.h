fcab23e88a890a2c49d44f166a8618403088f17f
David Gaden <dlfgaden@gmail.com>
2017-05-18 12:49:42 -0500

    Bringing in HashTable(http://pumpkinprogrammer.com/2014/06/21/c-tutorial-intro-to-hash-tables/), starting work on Dictionary

--- src/core/Dictionary.h
+++ src/core/Dictionary.h
@@ -1,1 +1,44 @@
-#ifndef 
+#ifndef Dictionary_h
+#define Dictionary_h
+
+// Forward declarations
+
+class Dictionary;
+std::ostream& operator<<(std::ostream& os, const Dictionary& c);
+std::istream& operator>>(std::istream& is, Dictionary& c);
+
+
+class Dictionary:
+	public std::list<Entry>
+{
+public:
+
+	// Constructors
+
+		//- Construct null
+		Dictionary();
+
+		//- Construct from istream
+		Dictionary(std::istream& is);
+
+	//- Destructor
+	~Dictionary();
+
+
+    // Friend functions
+    friend std::ostream& operator<<(std::ostream& out, const Dictionary& c);
+    friend std::istream& operator>>(std::istream& in, Dictionary& c);
+
+
+private:
+
+	// Private member data
+
+		//- Hashed table for quick look-up
+		HashTable<Entry*> hashedEntries_;
+
+		//- Parent dictionary
+		const Dictionary& parent_;
+};
+
+#endif
